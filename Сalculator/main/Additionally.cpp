6f2bcb9d2844f7ef30527f8c7b5425a5206dad69
Anna Zhuk <annazhuk2067@gmail.com>
2021-12-27 18:34:47 +0300

    file Testing updated

--- Сalculator/main/Additionally.cpp
+++ Сalculator/main/Additionally.cpp
@@ -17,16 +17,36 @@ int NOK(int a, int b)
     return ((a*b) / NOD(a, b));
 }
 int Additionally() {
+    char ElementValue[SIZE];
 	setlocale(LC_ALL, "RUS");
     char letmeout;
     int x, y;
     do {
         cout << "Введите последовательно 2 числа: " << endl;
-        cin >> x >> y;
+        cin >> ElementValue;
+        if (static_cast<int>(ElementValue[0]) > 57 || static_cast<int>(ElementValue[0]) < 48) {
+            printf("Ошибка - введен неправильный символ....\n");
+            system("pause");
+            return 0;
+        }
+        else  x = atoi(ElementValue);
+        cin >> ElementValue;
+        if (static_cast<int>(ElementValue[0]) > 57 || static_cast<int>(ElementValue[0]) < 48) {
+            printf("Ошибка - введен неправильный символ....\n");
+            system("pause");
+            return 0;
+        }
+        else  y = atoi(ElementValue);
         cout << "НОД этих чисел = " << NOD(x, y) << endl;
         cout << "НОК этих чисел = " << NOK(x, y) << endl;
         cout << "Чтобы повторить - введите любой символ. Чтобы закончить - введите '0'" << endl;
-        cin >> letmeout;
+        cin >> ElementValue;
+        if (static_cast<int>(ElementValue[0]) > 57 || static_cast<int>(ElementValue[0]) < 48) {
+            printf("Ошибка - введен неправильный символ....\n");
+            system("pause");
+            return 0;
+        }
+        else  letmeout = atoi(ElementValue);
         printf("\033c");
     } while (letmeout != '0');
     return 0;
